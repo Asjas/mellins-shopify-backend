2d1c433cfec08a9393733bfb8b50ba33e1dd79bc
A-J Roos <asjas@hey.com>
2021-08-11 16:53:00 +0200

    add a whole bunch of fastify plugins to the project

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,35 @@ import envSchema from "env-schema";
 import S from "fluent-json-schema";
 
 const schema = S.object()
+  .prop("NODE_ENV", S.string().default("production"))
   .prop("PORT", S.number().required())
   .prop("LOG_LEVEL", S.string().default("info"))
-  .prop("PRETTY_PRINT", S.boolean().default(false));
+  .prop("PRETTY_PRINT", S.boolean().default(false))
+  .prop("COOKIE_SECRET", S.string())
+  .prop("SESSION_SECRET", S.string())
+  .prop("SHOPIFY_HOST", S.string())
+  .prop("SHOPIFY_ACCESS_TOKEN", S.string())
+  .prop("SHOPIFY_GRAPHQL_PREFIX", S.string())
+  .prop("MAIL_HOST", S.string())
+  .prop("MAIL_PORT", S.number())
+  .prop("MAIL_USER", S.string())
+  .prop("MAIL_PASS", S.string());
 
 export type Config = {
   logger: boolean;
+  NODE_ENV: string;
   PORT: number;
   LOG_LEVEL: string;
   PRETTY_PRINT: boolean;
+  COOKIE_SECRET: string;
+  SESSION_SECRET: string;
+  SHOPIFY_HOST: string;
+  SHOPIFY_ACCESS_TOKEN: string;
+  SHOPIFY_GRAPHQL_PREFIX: string;
+  MAIL_HOST: string;
+  MAIL_PORT: number;
+  MAIL_USER: string;
+  MAIL_PASS: string;
 };
 
 export default envSchema({
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
-import fastify, { FastifyServerOptions } from "fastify";
+import Fastify, { FastifyServerOptions } from "fastify";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import FastifyHelmet from "fastify-helmet";
+import FastifyCors from "fastify-cors";
+import FastifyCookie from "fastify-cookie";
+import FastifySession from "fastify-session";
+import AutoLoad from "fastify-autoload";
+import FastifyNodemailer from "fastify-nodemailer";
+import FastifyHealthcheck from "fastify-healthcheck";
+import FastifyFavicon from "fastify-favicon";
+import ShopifyGraphQLProxy, { ApiVersion } from "fastify-shopify-graphql-proxy";
 
 import type { Config } from "./config";
 
@@ -16,23 +25,88 @@ async function createServer(config: Config) {
     ...config,
   };
 
-  const server = fastify(opts);
+  const server = Fastify(opts);
 
-  await server.register(import("fastify-favicon"));
-
-  await server.register(import("fastify-healthcheck"));
-
-  await server.register(import("under-pressure"), {
-    exposeStatusRoute: true,
-    maxEventLoopDelay: 1000,
-    maxHeapUsedBytes: 100000000,
-    maxRssBytes: 100000000,
-    maxEventLoopUtilization: 0.98,
+  await server.register(FastifyCors, {
+    origin: "*",
   });
 
-  await server.register(import("fastify-autoload"), {
+  await server.register(FastifyHelmet, {
+    frameguard: false,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        baseUri: ["'self'"],
+        connectSrc: ["'self'", "https:"],
+        fontSrc: ["'self'", "https:", "data:"],
+        frameAncestors: ["'self'", "https://online-mellins.myshopify.com"],
+        imgSrc: ["'self'", "data:"],
+        objectSrc: ["'self'"],
+        frameSrc: ["'self'"],
+        styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+        scriptSrc: ["'self'", "'unsafe-eval'"],
+        scriptSrcAttr: ["'self'"],
+      },
+    },
+  });
+
+  await server.register(FastifyFavicon);
+
+  await server.register(FastifyHealthcheck);
+
+  // await server.register(import("under-pressure"), {
+  //   maxEventLoopDelay: 1000,
+  //   maxHeapUsedBytes: 100000000,
+  //   maxRssBytes: 100000000,
+  //   maxEventLoopUtilization: 0.98,
+  // });
+
+  await server.register(FastifyCookie, {
+    secret: config.COOKIE_SECRET,
+  });
+
+  await server.register(FastifySession, {
+    secret: config.SESSION_SECRET,
+    cookieName: "mellins-shopify-backend-sess",
+    cookie: {
+      httpOnly: true,
+      secure: config.NODE_ENV === "production",
+      sameSite: "Lax",
+      maxAge: 1_800_0000,
+    },
+  });
+
+  await server.register(FastifyNodemailer, {
+    host: config.MAIL_HOST,
+    port: config.MAIL_PORT,
+    secure: false,
+    auth: {
+      user: config.MAIL_USER,
+      pass: config.MAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // @ts-ignore
+  await server.register(ShopifyGraphQLProxy, {
+    shop: config.SHOPIFY_HOST,
+    password: config.SHOPIFY_ACCESS_TOKEN,
+    version: ApiVersion.July21,
+    prefix: config.SHOPIFY_GRAPHQL_PREFIX,
+  });
+
+  await server.register(AutoLoad, {
+    dir: join(__dirname, "plugins"),
+    dirNameRoutePrefix: false,
+    forceESM: true,
+  });
+
+  await server.register(AutoLoad, {
     dir: join(__dirname, "routes"),
     dirNameRoutePrefix: false,
+    forceESM: true,
   });
 
   return server;
